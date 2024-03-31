5c83bb8c66164207758b1762d6bc60bf800ada12
Saroj Sah <saroj.sah@s2022.tu-chemnitz.de>
2024-03-31 20:05:45 +0200

    created routing pages

---
+++ frontend/src/pages/Signin/Signin.jsx
@@ -0,0 +1,12 @@
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+
+const Signin = () => {
+  return (
+    <div className="signin">
+      <Heading>This is signin page</Heading>
+    </div>
+  );
+};
+
+export default Signin;
---
+++ frontend/src/pages/Signup/Signup.jsx
@@ -0,0 +1,12 @@
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div className="signup">
+      <Heading>This is signup page</Heading>
+    </div>
+  );
+};
+
+export default Signup;
