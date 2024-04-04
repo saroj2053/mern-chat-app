3effc1e31acfaf0a88728ebffb73927894d482fc
Saroj Sah <saroj.sah@s2022.tu-chemnitz.de>
2024-04-04 11:41:36 +0200

    updated socket url

--- frontend/src/context/SocketContextProvider.jsx
+++ frontend/src/context/SocketContextProvider.jsx
@@ -10,7 +10,7 @@ const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chatapp-t6jh.onrender.com/", {
         query: {
           userId: authUser._id,
         },
